f399903134d3ffa0f35750a12398b032a709bee5
Nicholas Bryant <nbyellow14@yahoo.com>
2024-12-08 23:29:40 -0500

    Added base code

---
+++ salmander/src/App.svelte
@@ -0,0 +1,19 @@
+<body>
+<main id="background">
+  <p id="displayPop">
+    Hello!
+  </p>
+  <div>
+    <button type="button" id="my">Start</button>
+  </div>
+</main>
+
+<script>
+
+  document.getElementById("my").addEventListener("click", displayPop);
+  function displayPop()
+  {
+    document.getElementById("displayPop").innerHTML = "Nice job!";
+  }
+</script>
+</body>
